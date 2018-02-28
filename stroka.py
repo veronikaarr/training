a01a1e459a3d50c9c9d14b3ba534a8d14054c189
veronika <veronikaargasova@gmail.com>
2018-02-28 15:08:11 +0300

    1chapter

---
+++ stroka.py
@@ -0,0 +1,30 @@
+#строка
+'''x = 'qwe'
+y = 'wer'
+z = str('3.4')
+a = repr(3.4)
+b = x ^ y
+print(b)'''
+
+#список
+'''print([x**2 for x in range(100) if x % 23 == 0])
+list1 = [1,2,3]
+list2 = [1,4,5]
+print(list1[1])'''
+
+#кортежи
+'''x = 1, 2, 3
+print(x)'''
+
+#множества
+'''x = set('ver')
+y = set('ron')
+print(x|y) #как отсортировать всю эту херобору'''
+
+#списки
+x = {'x': '1',
+     'y': '2'}
+z=x.items()
+print(y)
+
+
