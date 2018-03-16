aac7b0c34d9e08130b48dfcf5e9534fc932f240e
veronika <veronikaargasova@gmail.com>
2018-03-17 01:54:30 +0300

    my university task:1.1

--- berhun/task1.py
+++ berhun/task1.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 Tc = 27
 Ts = 221
@@ -11,10 +13,29 @@ lamda = 1/Tc
 nu = 1/Ts
 a = lamda/nu
 n = 10
-P0 = 0
+Potk = list()
+Pobsl = list()
+nsr = list()
 
 Pn = [(math.pow(a, x) / math.factorial(x)) for x in range(0, n)]
-print(Pn)
 
-P0 = list(map(lambda y: (1 / sum((math.pow(a, x) / math.factorial(x)) for x in range(0, y))), range(1, n)))
-print(P0)
+Po = list(map(lambda y: (1 / sum(Pn[0:y])), range(1, n+1)))
+
+for x in range(0, n):
+    Potk.append(Pn[x] * Po[x])
+    Pobsl.append(1 - Potk[x])
+    if x != 0:
+        nsr.append(a*Pobsl[x]/x)
+
+Potk = list(map(lambda x:round(x,3), Potk))
+nsr = list(map(lambda x:round(x,3), nsr))
+
+x = np.linspace(1, n, 0.1)
+z = [x for x in range(1,n)]
+plt.plot(z, Potk[1:n], z, nsr[0:n], color='red', marker='o', linestyle='--', markerfacecolor='blue')
+for i,j in zip(z,Potk):
+    plt.text(i, j, str(j))
+for w,e in zip(z,nsr):
+    plt.text(w, e, str(e))
+plt.show()
+
