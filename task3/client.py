f1782bd26f0da7404ffa26c38697cbe813c86703
veronika <veronikaargasova@gmail.com>
2018-03-20 22:24:48 +0300

    ver.2

--- task3/client.py
+++ task3/client.py
@@ -1,26 +1,9 @@
 import zmq
-import sys
-
-port = "5556"
-if len(sys.argv) > 1:
-    port =  sys.argv[1]
-    int(port)
-
-if len(sys.argv) > 2:
-    port1 =  sys.argv[2]
-    int(port1)
-
 context = zmq.Context()
-print "Connecting to server..."
 socket = context.socket(zmq.REQ)
-socket.connect ("tcp://localhost:%s" % port)
-if len(sys.argv) > 2:
-    socket.connect ("tcp://localhost:%s" % port1)
+socket.connect("tcp://127.0.0.1:5000")
 
-#  Do 10 requests, waiting each time for a response
-for request in range (1,10):
-    print "Sending request ", request,"..."
-    socket.send ("Hello")
-    #  Get the reply.
-    message = socket.recv()
-    print "Received reply ", request, "[", message, "]"
+msg = "Hello World"
+socket.send(msg)
+print "Sending", msg
+msg_in = socket.recv()
--- task3/server.py
+++ task3/server.py
@@ -1,18 +1,8 @@
 import zmq
-import time
-import sys
-
-port = "5556"
-if len(sys.argv) > 1:
-    port =  sys.argv[1]
-    int(port)
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:%s" % port)
+socket.bind("tcp://127.0.0.1:5000")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv()
-    print "Received request: ", message
-    time.sleep (1)
-    socket.send("World from %s" % port)
+msg = socket.recv()
+print "Got", msg
+socket.send(msg)
