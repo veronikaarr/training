42de166b114a1d987a171fea09cbc402d0c45da5
veronika <veronikaargasova@gmail.com>
2018-03-20 18:08:00 +0300

    ver.3

--- task2/client.py
+++ task2/client.py
@@ -1,9 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# сообщения идут в ту очередь, binding key которой совпадает с routing key сообщения
+'''#!/usr/bin/env python
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
+
+channel.exchange_declare(exchange = 'logs',
+                         exchange_type = 'fanout')
+
+result = channel.queue_declare(exclusive = True)  #обеспечивает каждому клиенту новую очередь
+queue_name = result.method.queue
+
+channel.queue_bind(exchange = 'logs',
+                   queue = queue_name)  #имя очереди уникально
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
@@ -11,7 +22,40 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % (body,))  #получение сообщений - подписка
 
 channel.basic_consume(callback,
-                      queue='hello', #наша callback функция будет получать сообщения из очереди с именем hello
+                      queue=queue_name, #наша callback функция будет получать сообщения из очереди с именем hello
                       no_ack=True)
 
-channel.start_consuming() #запускаем бесконечную очередь
+channel.start_consuming() #запускаем бесконечную очередь'''
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs',
+                         exchange_type='direct')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+severities = sys.argv[1:]
+if not severities:  #здесь - если нет аргумента, записывать всё  написан образей того, что можно отправить
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.exit(1)
+
+for severity in severities: #
+    channel.queue_bind(exchange='direct_logs',
+                       queue=queue_name,  #имя очереди создаётся рандомно -?
+                       routing_key=severity) # но её ключ будет тот, который мы написали в параметре
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+channel.basic_consume(callback,
+                      queue=queue_name,  #callback функция будет получать сообщения из этой очереди
+                      no_ack=True)
+
+channel.start_consuming()
--- task2/server.py
+++ task2/server.py
@@ -1,16 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+'''#!/usr/bin/env python3
 import pika
+import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
                'localhost'))  #подключились к брокеру сообщений, находящемся на локальном хосте
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')  #создали очередь с именем хеллоу
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')  #создали точку обмена с именем logs и типом fanout - просто передаёт
+#сообщения подписавшимся клиентам
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')  #отправили сообщение в очередь через точку обмена exchange в очередь hello
+message = 'Hello, boy {}, I am just a message'.format(' '.join(sys.argv[1:]))
+
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)  #отправили сообщение в очередь через точку обмена exchange в очередь hello
 #c телом Hello Worls - ?
 
-print(" [x] Sent 'Hello World!'") #
+print(" [x] Sent %r" % message) #
+connection.close()'''
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs',
+                         exchange_type='direct')
+
+severity = sys.argv[1] if len(sys.argv) > 2 else 'info' #если есть что-то в аргументе записывать в переменную, иначе -
+#она равна info
+message = ' '.join(sys.argv[2:]) or 'Hello World!' #сообщение - срез после второго аргумента - ?
+
+channel.basic_publish(exchange='direct_logs',
+                      routing_key=severity,
+                      body=message)
+print(" [x] Sent %r:%r" % (severity, message))
 connection.close()
