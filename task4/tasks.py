13572ea73bd8627e7ea74feab42de4fea5638906
veronika <veronikaargasova@gmail.com>
2018-03-21 14:47:41 +0300

    ver.task4

--- task4/tasks.py
+++ task4/tasks.py
@@ -1,52 +1,43 @@
-'''from celery import Celery
-
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
-
-@app.task
-def add(x, y):
-    return x + y'''
+#celery -A tasks worker --loglevel=info
+#python3 tasks1.py
 
 from celery import Celery
-import redis
-import time
 import subprocess
+import psutil
 
 app = Celery('tasks')
-app.config_from_object('celeryconfig')
+file = open('log.txt', 'w')
 
 # -----------------------------------------------------------------------------
-
+@app.task
 def fib_slow(n):
     if n < 0:
         return 0
     if n in (0,1):        return 1
     return fib(n-1) + fib(n-2)
 
-def fib_fast(n):
-    s = subprocess.check_output(['./fib', str(n)])
-    return s
-
-def fib_to_file(n, filename):
-    with open(filename, 'w') as outfile:
-        cmd = ['./fib', str(n)]
-        subprocess.call(cmd, stdout=outfile)
-        sys.stderr.write('Wrote ' + outfile.name + '\n')
-    return True
-
 # -----------------------------------------------------------------------------
 
 @app.task
-def add(x, y):
-    if True:
-        time.sleep(10)
-    return x + y
+def subpr():
+    cmd = subprocess.Popen(['ls', '-l'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out,error = cmd.communicate()
+    memory = out.splitlines()
+
+    for line in memory:
+        file.write(str(line.decode('utf-8')) + '\n')
+        #print(line.decode('utf-8'))
+
+    o = psutil.cpu_times(True)
+
+    for lines in o:
+        file.write(str(lines) + '\n')
+        #print(lines)
 
 @app.task
 def fib(n):
-    f = fib_fast(n)
-    if True:
-        time.sleep(10)
-    if True:
-        r = redis.Redis('localhost')
-        r.publish('tasks:all', f)
-    return n
+    f = fib_slow(n)
+    subpr()
+    print(f)
+    return f
+
--- task4/tasks1.py
+++ task4/tasks1.py
@@ -1,22 +1,7 @@
-import subprocess
-import os
-import psutil
+from tasks import fib
+from tasks import subpr
+from random import randint
 
-cmd = subprocess.Popen(['ls', '-l'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-out,error = cmd.communicate()
-memory = out.splitlines()
-
-for line in memory:
-    print(line.decode('utf-8'))
-
-"""CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
-
-print("CPU Usage = " + CPU_Pct)
-
-tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
-print(tot_m, used_m, free_m)"""
-
-o = psutil.cpu_times(True)
-
-for lines in o:
-    print(lines)
+n = randint(30,40)
+fib.delay(n)
+subpr.delay()
