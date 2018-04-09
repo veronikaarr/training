7e12fa02f501e0149f7709b53cb494fee1f6ecb7
veronika <veronikaargasova@gmail.com>
2018-04-09 21:42:03 +0300

    with comment without iter not work

--- berhun/task2.py
+++ berhun/task2.py
@@ -12,31 +12,54 @@ with open('82-1.csv', "r", encoding='utf-8') as f:
     your_list = list(reader)
     for i in range(len(your_list)):
         your_list[i] = list(map(int, your_list[i])) #текст из csv сделали цифрами
-for_del = 0
+
 sigma = []; for_del_ind = []
 
 def find_max_min():
+    q=0
+    q1 = 0
     sum_val = []
-    indexs = [x for x, y in notnull]  #записываем индексы изначальной строки
-    notnull.pop()  #после удаляем индекс изнач строки
-    notnull_sum = [e for i, e in enumerate(values_sum) if i in indexs] #записываем все суммы строк с этими индексами
-    notnull_sum.remove(value_min_sum)
-    for j in range(len(notnull)):  #проходимся по строкам с этими индексами
+    print(for_del,'etoooo for deeeeeel\n')
+    print(notnull,'notnuuul after for')
+    '''если в предыдущий вызов найден эл-т для удаления'''
+    if for_del != 0:
+        '''обходим циклом по всему notnull'''
+        for x1, x2 in notnull:
+            q = q + 1
+            print(x1,'etoooooo x1')
+            print(for_del,'etoooo for deeeeeel\n')
+            if for_del == x1:
+                q1 = q - 1
+                print(q1, 'qqqqqqqqqq1\n')
+                print(notnull,'notnuliiiiidze\n')
+                del notnull[q1]
+                del notnull_sum[q1]
+                print('miii zdes\n')
+    print(for_del,'fooooordeeeel\n')
+    print(notnull, 'noooootnuuul\n')
+    print(notnull_sum, 'notnuuuuuuuulsuum\n')
+        #notnull[for_del]
+      #  notnull_sum.remove(for_del_value)
+    '''проходимся по строкам с этими индексами'''
+    for j in range(len(notnull)):
         index, value = notnull[j]
         for_sum = [e for i, e in enumerate(your_list[index]) if i in indexs]
-        x = sum(for_sum) #записываем сумму элементов
-        sum_val.append(x)  #добавляем в общий список
+        '''записываем сумму элементов'''
+        x = sum(for_sum)
+        '''добавляем в общий список'''
+        sum_val.append(x)
     print('kogo vihit:',notnull_sum)
     print('hto vihit:',sum_val)
-    sigma = list(map(lambda a, b: a - b, notnull_sum, sum_val)) #считаем разность сумм
+    '''считаем разность сумм'''
+    sigma = list(map(lambda a, b: a - b, notnull_sum, sum_val))
     print('Сигма',sigma)
-
     sigma_index = [(i,e) for i,e in zip(indexs,sigma)]
    # print("res vihit:",sigma_index)
     if sign == 1:
         ind,val = max(sigma_index, key=lambda x: x[1])
     else:
         ind,val = min(sigma_index, key=lambda x: x[1])
+    print('Максимальное значение {0} для эл-та {1} \n'.format(val, ind))
     return (ind, indexs)
 
 def find_min_string():
@@ -49,8 +72,14 @@ def find_min_string():
     notnull = [(i, e) for i, e in enumerate(your_list[index_min]) if e != 0] #записываем все ненулевые эл-ты
     notnull.append((index_min, your_list[index_min]))
     print('Индексы смежных элементов:', [i for i, e in notnull if i != index_min])
+    indexs = [x for x, y in notnull]  #записываем индексы изначальной строки
+    print(notnull,'notnnnnnnnnnul')
+    notnull.pop()  #после удаляем индекс изнач строки
+    print(notnull,'notnnnnnnnnnul2')
+    notnull_sum = [e for i, e in enumerate(values_sum) if i in indexs] #записываем все суммы строк с этими индексами
+    notnull_sum.remove(value_min_sum)
     # из неё в список
-    return (values_sum,notnull,value_min_sum)
+    return (values_sum, notnull, value_min_sum, indexs, notnull_sum)
 
 #your_list = np.array(your_list)
 groups = [4,5,7,7,7]
@@ -58,24 +87,34 @@ groups = [4,5,7,7,7]
 
 print(np.array(your_list))
 print('\n')
+for_del = 0
+qwe = 0
 
+'''проход по всем группам'''
 for j in groups:
     print('etoooo j:',j)
-    values_sum, notnull, value_min_sum = find_min_string()
-    while(len(notnull) != j):
-       # print(j,'eeetoooo takoe j')
-        sign = np.sign(len(notnull) - j) #узнаем, больше или меньше число эл-ов в строке, чем очередное число из списка гр-п
-        print(sign,'siiign')
+    '''поиск миним элемента в группе'''
+    values_sum, notnull, value_min_sum, indexs, notnull_sum = find_min_string()
+    print(notnull,'notnuuuuul in for')
+    '''пока кол-во эл-ов в группе не станет равным заданному числу'''
+    while(qwe != 7):
+        qwe = qwe +1
+        print(j,'etooooo j\n')
+        print(len(notnull),'etoooooooo len\n')
+        sign = np.sign(len(notnull) - j) #узнаем, больше или меньше число эл-ов в строке, чем очередное
+        # число из списка гр-п
+        print(sign,'siiign\n')
+        '''находим минимальное или максимальное число, в зависимости от sigma'''
         for_del, indexss = find_max_min()
         print(for_del)
+        '''удалённые элементы добавляем в список'''
         for_del_ind.append(for_del)
        # print('do delete:',np.array(your_list))
        # print('eto indexs:',indexss)
-        for i in range(len(your_list[for_del])):
-            your_list[for_del][i] = 0
-            your_list[i][for_del] = 0
+        for p in range(len(your_list[for_del])):
+            your_list[for_del][p] = 0
+            your_list[p][for_del] = 0
         #    print('eto for_del:', for_del, i)
-        values_sum, notnull,value_min_sum = find_min_string()
        # print('after del:')
         #for k in range(len(your_list)):
            # if your_list[k]:
@@ -83,6 +122,6 @@ for j in groups:
         print('\n')
     print('First groop------------------------------------:', [i for i,e in notnull])
     for h in range(len(your_list[for_del])):
-        for i in for_del_ind:
-            your_list[i][h] = 0
-            your_list[h][i] = 0
+        for o in for_del_ind:
+            your_list[o][h] = 0
+            your_list[h][o] = 0
